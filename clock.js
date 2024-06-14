4d2aae0824fe6ec4a3a742f257925a819782d751
jaredcourage <145121823+jaredcourage@users.noreply.github.com>
2024-06-14 09:47:07 +0200

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,145 @@
+var num1 = document.getElementById('num-1').innerText = '+' + "1";
+console.log(num1);
+
+var num2 = document.getElementById('num-2').textContent = "+" + "2";
+console.log(num2);
+
+var num3 = document.getElementById('num-3').textContent = "+" + "3";
+console.log(num3);
+
+var numBUN = document.getElementById('num-bun').textContent = "reset"
+
+console.log(numBUN);
+
+
+
+
+var num4 = document.getElementById('num-4').textContent = "+" + "1";
+console.log(num4);
+
+var num5 = document.getElementById('num-5').textContent = "+" + "2";
+console.log(num5);
+
+var num6 = document.getElementById('num-6').textContent = "+" + "3";
+console.log(num6);
+
+var numBUN1 = document.getElementById('num-bun1').textContent = "reset"
+
+console.log(numBUN1);
+
+var chuck3 = document.getElementById('chuck-3');
+console.log(chuck3);
+
+
+var mega3 = document.getElementById('mega-3');
+console.log(mega3);
+
+var bonkEL = document.getElementById('bonk-el').textContent = "Period";
+console.log(bonkEL);
+
+var foulEL = document.getElementById('foul-el').textContent = "Foul";
+console.log(foulEL);
+
+var parkEL = document.getElementById('park-el').textContent = "Period";
+console.log(parkEL);
+
+var foul2 = document.getElementById('foul-2').textContent = "Foul";
+console.log(foul2);
+
+var timeEL = document.getElementById('time-el').textContent = "Time";
+console.log(timeEL);
+
+var time2 = document.getElementById('time-2').textContent = "Time";
+console.log(time2);
+
+
+
+
+
+
+
+
+
+function add1() {
+    console.log('button was clicked')
+    chuck3.textContent = 13
+}
+
+function add2() {
+    console.log('button was clicked')
+    chuck3.textContent = 14
+}
+
+
+function add3() {
+    console.log('button was clicked')
+    chuck3.textContent = 15
+}
+
+
+
+function addbun() {
+    console.log('button was clicked');
+    chuck3.textContent = 0
+}
+
+function addbun1() {
+    console.log('button was clicked');
+    mega3.textContent = 0
+}
+
+
+
+function add4() {
+    console.log('button was clicked')
+    mega3.textContent = 6
+}
+
+function add5() {
+    console.log('button was clicked')
+    mega3.textContent = 7
+}
+
+
+function add6() {
+    console.log('button was clicked')
+    mega3.textContent = 8
+}
+
+function period() {
+    console.log('the button was clicked');
+    chuck3.textContent = "1st"
+}
+
+function foul() {
+    console.log('the button was clicked');
+    chuck3.textContent = "H:" + "6"
+}
+
+function timeo() {
+    console.log('the button was clicked');
+    chuck3.textContent = "4:23";
+}
+
+
+
+
+
+
+function park() {
+    console.log('the button was clicked');
+    mega3.textContent = "2nd"
+}
+
+function see(){
+    console.log('the button was clicked');
+    mega3.textContent = "G:" + "1"
+}
+
+function minute() {
+    console.log('the button was clicked');
+    mega3.textContent = "1:00" ;
+}
+
+
+
